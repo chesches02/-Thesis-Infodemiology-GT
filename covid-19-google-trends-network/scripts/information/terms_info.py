9472b5ffb687c4e7eab3cdb5ac4f12b6b695e80d
chesches02 <cheska.hung@student.ateneo.edu>
2024-09-01 19:02:07 +0800

    got correlation coefficients

---
+++ covid-19-google-trends-network/scripts/information/terms_info.py
@@ -0,0 +1,18 @@
+tags = [
+    "flu",
+    "cough",
+    "fever",
+    "headache",
+    "lagnat",
+    "rashes",
+    "sipon",
+    "ubo",
+    "ecq",
+    "face-shield",
+    "Frontliners",
+    "masks",
+    "Quarantine",
+    "social-distancing",
+    "work-from-home",
+
+]
